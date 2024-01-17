a07b6b9674e82940a49e32cf1deefe62e7b8e649
rolins2 <127750238+rolins2@users.noreply.github.com>
2024-01-18 01:27:43 +0530

    Fwat: added lsiten functionality to the server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-import express from "express";
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+import bodyParser from "body-parser";
+const port = 3000;
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+})
