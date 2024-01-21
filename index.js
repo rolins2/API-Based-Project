d1d414314a1133ad57dd8871dc2fc7c439167280
rolins2 <127750238+rolins2@users.noreply.github.com>
2024-01-21 21:14:20 +0530

    Feat : App launch

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 import bodyParser from "body-parser";
 const port = 3000;
 
+const url  = "https://api.weatherbit.io/v2.0/current";
+const api_key = "37e9beda33594207a415b790b51fc226"; 
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
 })
