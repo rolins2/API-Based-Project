de5c8eb54995c286491ffeec747af000e7987ed0
rolins2 <127750238+rolins2@users.noreply.github.com>
2024-01-21 23:46:48 +0530

    Feat: Added body parser

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import axios from 'axios';
+import bodyParser from "body-parser";
+
 
 const app = express();
-import bodyParser from "body-parser";
 const port = 3000;
 
 const url  = "https://api.weatherbit.io/v2.0/current";
 const api_key = "37e9beda33594207a415b790b51fc226"; 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
+})
+
+app.post("/getD",(req,res)=>{
+    console.log("this point has been hit");
+    console.log(req.body);
 })
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
